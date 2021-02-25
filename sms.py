15549bf94142244b83f2472dee9afe6e7c52d00e
Muchezz <muchezz@outlook.com>
2021-02-26 00:24:18 +0300

    Intiial Commit

---
+++ sms.py
@@ -0,0 +1,14 @@
+# Program extracting all columns
+# name in Python
+import xlrd
+
+loc = ("path of file")
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+
+# For row 0 and column 0
+sheet.cell_value(0, 0)
+
+for i in range(sheet.ncols):
+	print(sheet.cell_value(0, i))
