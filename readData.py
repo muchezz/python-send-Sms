c7dae3323833e490d24043d3488412830110df3d
Muchezz <muchezz@outlook.com>
2021-03-08 22:41:34 +0300

    Read Data From Sheet

--- readData.py
+++ readData.py
@@ -1,16 +1,18 @@
 # Program extracting all columns
 # name in Python
 import openpyxl
+import datetime
 
 # Give the location of the file
-loc = ("produceSales.xlsx")
+loc = ("mwFellowship.xlsx")
  
 # To load entire Workbook
-wb = openpyxl.load_workbook(loc)
+wb = openpyxl.load_workbook(loc, data_only=True)
 
 # Load one worksheet.
 ws = wb['Sheet1']
 all_rows = list(ws.rows)
+#all_colm = list(ws.columns)
 
 # List all the sheets in the file.
 #print("Found the following worksheets:")
@@ -24,6 +26,21 @@ all_rows = list(ws.rows)
 #for row in all_rows[:500]:
 #    print(row)
 
+# Generating Week Numbers
+myDate = datetime.date.today()
+year, week_num, day_of_week = myDate.isocalendar()
+#print("This is week # " +str(week_num) + " Date " +str(day_of_week))
+
 #Read the data in the rows
-for cell in all_rows[1]:
-    print(cell.value)
+sheetWeekNo = []
+for row in ws.rows:
+    sheetWeekNo.append (row[0].value)
+    #print(row[0].value)
+    
+
+for i in sheetWeekNo: 
+    if(i == week_num) : 
+        print ("Element Exists") 
+    
+
+
--- sendSms.py
+++ sendSms.py
@@ -2,15 +2,14 @@ import os
 from twilio.rest import Client
 
 
-account_sid = ' ',
+account_sid = ''
 auth_token = ''
 #account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
 #auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 
 client = Client(account_sid, auth_token)
 
-message =client.messages.create(from_='',
-                      to='',
-                      body='You just sent an SMS from Python using Twilio!')
+message =client.messages.create(from_='+17076402663',
+                      to='+254714869114',
+                      body='Hi Man, whats up')
 
-print(message.sid)
