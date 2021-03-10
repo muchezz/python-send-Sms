c19406943a06c71d951ce8c9db36dc8686633454
Muchezz <muchezz@outlook.com>
2021-03-10 22:38:11 +0300

    Read Data from Sheet: Update

--- readData.py
+++ readData.py
@@ -1,46 +1,16 @@
-# Program extracting all columns
-# name in Python
-import openpyxl
-import datetime
-
-# Give the location of the file
-loc = ("mwFellowship.xlsx")
- 
-# To load entire Workbook
-wb = openpyxl.load_workbook(loc, data_only=True)
-
-# Load one worksheet.
-ws = wb['Sheet1']
-all_rows = list(ws.rows)
-#all_colm = list(ws.columns)
-
-# List all the sheets in the file.
-#print("Found the following worksheets:")
-#for sheetname in wb.sheetnames:
-#    print(sheetname)
+from datetime import datetime
+import pandas as pd
 
 
-#print the first few rows of data:
-#print(f"Found {len(all_rows)} rows of data.")
-#print("\nFirst rows of data:")
-#for row in all_rows[:500]:
-#    print(row)
+data = pd.read_excel('mwFellowship.xlsx',engine='openpyxl')
 
-# Generating Week Numbers
-myDate = datetime.date.today()
-year, week_num, day_of_week = myDate.isocalendar()
-#print("This is week # " +str(week_num) + " Date " +str(day_of_week))
+schedules = data.to_dict('records')
 
-#Read the data in the rows
-sheetWeekNo = []
-for row in ws.rows:
-    sheetWeekNo.append (row[0].value)
-    #print(row[0].value)
-    
+# date processing
+current_data = datetime.now().date()
+year, week_num, day_of_week = current_data.isocalendar()
 
-for i in sheetWeekNo: 
-    if(i == week_num) : 
-        print ("Element Exists") 
-    
-
-
+for schedule in schedules:
+    if schedule['Week'] == week_num:
+        text_msg =pd.DataFrame.from_dict(schedule, orient='index')
+        print(text_msg)
--- sendSms.py
+++ sendSms.py
@@ -1,4 +1,5 @@
 import os
+import readData
 from twilio.rest import Client
 
 
