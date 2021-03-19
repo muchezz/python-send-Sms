b448793d8ec7e246a6773ea004bd40146a477dcd
Muchezz <muchezz@outlook.com>
2021-03-19 22:52:12 +0300

    Update on SendSms.py

--- sendSms.py
+++ sendSms.py
@@ -3,21 +3,22 @@ from readData import *
 from twilio.rest import Client
 
 
-account_sid = ''
-auth_token = ''
-#account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-#auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+def send_sms( event = None, context= None):
+    
 
-client = Client(account_sid, auth_token)
+    account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+    auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+
+    client = Client(account_sid, auth_token)
 
 #message =client.messages.create(from_='+17076402663',
                      # to='+254714869114',
                       #body='Hi Man, whats up')
 
-message = client.messages \
-    .create(
-         from_='+17076402663',
-         body="A happy new week! Here is a list of people on duty this week 🙏 {} See you on Thur at 7pm".format(text_msg),
-         to='+254712423207'
-     )
+    message = client.messages \
+        .create(
+             from_='+17076402663',
+             body="A happy new week! Here is a list of people on duty this week 🙏 \n {}  \n See you on Thur at 7pm".format(text_msg),
+             to='+254714869114'
+         )
 
